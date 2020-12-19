a62fe81780d4c25ea5458d4996e3e35666fc8c03
Noel-Garces08 <noel.garces08@gmail.com>
2020-12-18 20:40:21 -0700

    Create feature product carousel

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,30 @@
-const carouselSlider = document.querySelector('.carousel__slider');
-const carouselItems = document.querySelectorAll('.carousel__item');
-const carouselControlLeft = document.querySelector('.carousel__control--left');
-const carouselControlRight = document.querySelector('.carousel__control--right');
-
-const width = carouselItems[0].offsetWidth;
-
-let counter = 0;
-carouselControlRight.addEventListener('click', () => {
-    if (counter === carouselItems.length - (window.innerWidth === 768 ? 4 : 2)) return;
-    counter++;
-    carouselSlider.style.transform = `translateX(${-width * counter}px)`;
-});
-
-carouselControlLeft.addEventListener('click', () => {
-    if (counter === 0) return;
-    counter--;
-    carouselSlider.style.transform = `translateX(${-width * counter}px)`;
+$(document).ready(function () {
+    $('.carousel__slider').slick({
+        infinite: false,
+        prevArrow: '<i class="fas fa-chevron-left"></i>',
+        nextArrow: '<i class="fas fa-chevron-right"></i>',
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 });
